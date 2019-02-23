8383b9de9ccd18295d19f87a6eaf116a53005df4
Vincent18zt <qq754159742@outlook.com>
2019-02-23 20:25:37 +0800

    js移植

--- back-end/High-speed rail shared logistics platform system/WebContent/js/1.js
+++ back-end/High-speed rail shared logistics platform system/WebContent/js/1.js
@@ -48,6 +48,14 @@ function staff_addSub(){
 		alert("员工卡号不正确！");
 		return false;
 	}
+	else if (staffSalary.length > 10){
+		alert("员工工资数目大于10！");
+		return false;
+	}
+	else if (staffCard.length > 20){
+		alert("员工卡号数目大于20！");
+		return false;
+	}
 	else {
 		alert("提交成功！");
 		return true;
@@ -60,13 +68,47 @@ function staff_editSub(){
 	var staffSex = document.getElementById("sex").value;
 	var staffSalary = document.getElementById("salary").value;
 	var staffCard = document.getElementById("card").value;
-	
+	var reg_num = /^\d+$/;
+
+	console.log(!reg_num.test(staffCard) + "dick")
 	if (staffName == ""){
 		alert("员工姓名不能为空！");
 		return false;
 	}
 	else if (staffPos == ""){
 		alert("员工职务不能为空！");
+		return false;
+	}
+	else if (staffName.length > 10){
+		alert("员工姓名长度过长！");
+		return false;
+	}
+	else if (staffPos.length > 10){
+		alert("员工职务长度过长！");
+		return false;
+	}
+	else if (staffSalary == ""){
+		alert("员工薪水不能为空！");
+		return false;
+	}
+	else if (staffCard == ""){
+		alert("员工银行卡不能为空！");
+		return false;
+	}
+	else if (!reg_num.test(staffSalary)){
+		alert("员工工资不为整数！");
+		return false;
+	}
+	else if (!reg_num.test(staffCard)){
+		alert("员工卡号不正确！");
+		return false;
+	}
+	else if (staffSalary.length > 10){
+		alert("员工工资数目大于10！");
+		return false;
+	}
+	else if (staffCard.length > 20){
+		alert("员工卡号数目大于20！");
 		return false;
 	}
 	else {
@@ -85,31 +127,57 @@ function goods_editSub(){
 	var order = document.getElementById("order").value;
 	var orderFinished = document.getElementById("order_finished").value;
 	
+	var reg_num = /^\d+$/;
+	var reg_float = /^\d+(\.\d+)?$/;
+
 	if (goodsID == ""){
 		alert("物品ID不能为空！");
 		return false;
 	}
-	
 	else if (senderID == ""){
 		alert("下单用户ID不能为空！");
 		return false;
 	}
-	
+	else if (!reg_num.test(senderID)){
+		alert("下单用户ID不为整数！");
+		return false;
+	}
+	else if (senderID.length > 10) {
+		alert("下单用户ID长度大于10！");
+		return false;
+	}
 	else if (receiverID == ""){
 		alert("接单用户ID不能为空！");
 		return false;
 	}
-	
+	else if (!reg_num.test(receiverID)){
+		alert("接单用户ID不为整数！");
+		return false;
+	}
+	else if (receiverID.length > 10) {
+		alert("接单用户ID长度大于10！");
+		return false;
+	}
 	else if (trainNum == ""){
 		alert("车次不能为空！");
 		return false;
 	}
-	
+	else if (trainNum.length > 10) {
+		alert("车次长度大于10！");
+		return false;
+	}
 	else if (fee == ""){
 		alert("运费不能为空！");
 		return false;
 	}
-	
+	else if (!reg_float.test(fee)){
+		alert("运费不为数字！");
+		return false;
+	}
+	else if (fee.length > 10) {
+		alert("车次长度大于10！");
+		return false;
+	}
 	else{
 		alert("提交成功！");
 		return true;
@@ -127,31 +195,57 @@ function goods_addSub(){
 	var order = document.getElementById("order_1").value;
 	var orderFinished = document.getElementById("order_finished_1").value;
 	
+	var reg_num = /^\d+$/;
+	var reg_float = /^\d+(\.\d+)?$/;
+
 	if (goodsID == ""){
 		alert("物品ID不能为空！");
 		return false;
 	}
-	
 	else if (senderID == ""){
 		alert("下单用户ID不能为空！");
 		return false;
 	}
-	
+	else if (!reg_num.test(senderID)){
+		alert("下单用户ID不为整数！");
+		return false;
+	}
+	else if (senderID.length > 10) {
+		alert("下单用户ID长度大于10！");
+		return false;
+	}
 	else if (receiverID == ""){
 		alert("接单用户ID不能为空！");
 		return false;
 	}
-	
+	else if (!reg_num.test(receiverID)){
+		alert("接单用户ID不为整数！");
+		return false;
+	}
+	else if (receiverID.length > 10) {
+		alert("接单用户ID长度大于10！");
+		return false;
+	}
 	else if (trainNum == ""){
 		alert("车次不能为空！");
 		return false;
 	}
-	
+	else if (trainNum.length > 10) {
+		alert("车次长度大于10！");
+		return false;
+	}
 	else if (fee == ""){
 		alert("运费不能为空！");
 		return false;
 	}
-	
+	else if (!reg_float.test(fee)){
+		alert("运费不为数字！");
+		return false;
+	}
+	else if (fee.length > 10) {
+		alert("车次长度大于10！");
+		return false;
+	}
 	else{
 		alert("提交成功！");
 		return true;
@@ -166,32 +260,46 @@ function vip_editSub(){
 	var phoneNum = document.getElementById("phone_num").value;
 	var pin = document.getElementById("pin").value;
 	var address = document.getElementById("address").value;
+
+	var reg_pin = /^\d{15}|\d{}18$/
+	var reg_phone = /^1\d{10}$/
 	
 	if (vipID == ""){
 		alert("用户ID不能为空！");
 		return false;
 	}
-	
 	else if (vipName == ""){
 		alert("用户姓名不能为空！");
 		return false;
 	}
-	
+	else if (vipName.length > 10){
+		alert("用户姓名过长！");
+		return false;
+	}
 	else if (phoneNum == ""){
 		alert("用户电话号码不能为空！");
 		return false;
 	}
-	
+	else if (!reg_phone.test(phoneNum)){
+		alert("用户电话号码格式错误！");
+		return false;
+	}
 	else if (pin == ""){
 		alert("用户身份证号码不能为空！");
 		return false;
 	}
-	
+	else if (!reg_pin.test(pin)){
+		alert("用户身份证号码格式错误！");
+		return false;
+	}
 	else if (address == ""){
 		alert("用户地址不能为空！");
 		return false;
 	}
-	
+	else if (address.length > 25){
+		alert("用户地址大于25！");
+		return false;
+	}
 	else{
 		alert("提交成功！");
 		return true;
